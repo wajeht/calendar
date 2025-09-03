c7030e40b9e3c594266de0de98a5bca92c027886
wajeht <58354193+wajeht@users.noreply.github.com>
2025-09-03 00:57:49 -0500

    feat(src): add static file serving to the hono application

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
+import { serveStatic } from 'hono/bun'
 import { Hono } from 'hono'
 
 const app = new Hono()
+
+app.use(serveStatic({
+    root: './public'
+}))
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
-export default app
+export default { 
+  port: 80, 
+  fetch: app.fetch, 
+}
