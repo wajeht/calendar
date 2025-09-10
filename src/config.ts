938eee1b833c0ab49324e2dd7182f82c0a2590b1
wajeht <58354193+wajeht@users.noreply.github.com>
2025-09-10 11:45:15 -0500

    feat(calendar): implement calendar editing functionality with UI enhancements for editing and saving calendar details

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   APP_ENV: Bun.env.APP_ENV || 'development' as 'development' | 'production',
-  APP_PORT: Bun.env.APP_PORT || 80,
+  APP_PORT: Bun.env.APP_PORT || 3000,
   APP_DATABASE_PATH: Bun.env.APP_DATABASE_PATH || 'calendars.db',
   APP_PASSWORD: Bun.env.APP_PASSWORD || 'password',
 }
