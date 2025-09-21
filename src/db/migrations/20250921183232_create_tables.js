4e95348029bd21ec9d77dda712003a0a90065e5e
wajeht <58354193+wajeht@users.noreply.github.com>
2025-09-21 13:34:10 -0500

    refactor(db): consolidate calendar and settings table migrations into a single migration file, adding new fields and indexes for improved data structure

---
+++ src/db/migrations/20250921183232_create_tables.js
@@ -0,0 +1,44 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+    return knex.schema
+        .createTable("calendars", function (table) {
+            table.increments("id").primary();
+            table.text("name").notNullable().defaultTo("");
+            table.text("url").notNullable().unique();
+            table.text("color").notNullable().defaultTo("#447dfc");
+            table.boolean("hidden").notNullable().defaultTo(false);
+            table.boolean("details").notNullable().defaultTo(false);
+            table.text("data");
+            table.text("events");
+            table.text("events_public");
+            table.text("events_authenticated");
+            table.timestamps(true, true);
+
+            table.index("url");
+            table.index("hidden");
+            table.index("created_at");
+            table.index("updated_at");
+        })
+        .createTable("settings", (table) => {
+            table.string("key").primary();
+            table.text("value");
+            table.timestamp("created_at").defaultTo(knex.fn.now());
+            table.timestamp("updated_at").defaultTo(knex.fn.now());
+
+            table.index("created_at");
+            table.index("updated_at");
+        });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+    return knex.schema
+        .dropTableIfExists("settings")
+        .dropTableIfExists("calendars");
+}
