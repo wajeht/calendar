5f035e7867e8e1bd7c675fdb70d23a5591a5e71d
wajeht <58354193+wajeht@users.noreply.github.com>
2025-09-23 13:41:07 -0500

    fix(assets): update asset references in index.html and add new CSS for toast and modal components to enhance user experience

--- src/vue/components/Button.vue
+++ src/vue/components/Button.vue
@@ -79,22 +79,18 @@ const buttonClasses = computed(() => {
         >
             <svg
                 class="animate-spin h-4 w-4 text-current"
+                viewBox="0 0 50 50"
                 xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
             >
                 <circle
-                    class="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
+                    cx="25"
+                    cy="25"
+                    r="20"
+                    fill="none"
                     stroke="currentColor"
                     stroke-width="4"
-                />
-                <path
-                    class="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+                    stroke-linecap="round"
+                    stroke-dasharray="44 125"
                 />
             </svg>
         </span>
