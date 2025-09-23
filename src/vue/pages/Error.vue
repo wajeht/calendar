5cd5790d952f7c76e4d5a3b3b8d0c144a19fe142
wajeht <58354193+wajeht@users.noreply.github.com>
2025-09-23 00:15:50 -0500

    style(Error.vue): enhance error page layout and add footer with copyright information for improved user experience

--- src/vue/pages/Error.vue
+++ src/vue/pages/Error.vue
@@ -1,10 +1,17 @@
 <script setup></script>
 
 <template>
-    <main class="flex h-screen w-screen justify-center items-center">
-        <div class="flex gap-5">
-            <h1 class="font-bold">404</h1>
-            <p>The page you are looking for could not be found.</p>
-        </div>
-    </main>
+  <main class="flex flex-col h-screen w-screen justify-center items-center p-5 text-center">
+    <div class="flex items-center gap-3 flex-1 justify-center">
+      <h1 class="font-bold pr-6 border-r">404</h1>
+      <p class="pl-3 ">The page you are looking for could not be found.</p>
+    </div>
+
+    <footer class="mt-auto">
+      <p>
+        Copyright © 2025. Made with ❤️ by
+        <a href="https://github.com/wajeht" class="hover:underline">@wajeht</a>
+      </p>
+    </footer>
+  </main>
 </template>
