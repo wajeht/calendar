b8e9c48100b0c61d6be20123c34aa9ff99a61177
wajeht <58354193+wajeht@users.noreply.github.com>
2025-11-13 11:00:07 -0600

    feat(vue): add input ids to calendar modal form elements

--- src/vue/components/modals/ModalAddCalendar.vue
+++ src/vue/components/modals/ModalAddCalendar.vue
@@ -85,32 +85,36 @@ function handleClose() {
     <Modal title="Add New Calendar" :high-z-index="props.highZIndex" @close="handleClose">
         <form @submit.prevent="handleSubmit">
             <div class="space-y-4">
-                <FormGroup label="Name" required :error="errors.name">
+                <FormGroup label="Name" required input-id="calendar-name" :error="errors.name">
                     <Input
+                        id="calendar-name"
                         v-model="newCalendar.name"
                         type="text"
                         placeholder="Calendar name"
                         required
                     />
                 </FormGroup>
-                <FormGroup label="URL" required :error="errors.url">
+                <FormGroup label="URL" required input-id="calendar-url" :error="errors.url">
                     <Input
+                        id="calendar-url"
                         v-model="newCalendar.url"
                         type="url"
                         placeholder="https://example.com/calendar.ics"
                         required
                     />
                 </FormGroup>
-                <FormGroup label="Color" :error="errors.color">
-                    <Input v-model="newCalendar.color" type="color" />
+                <FormGroup label="Color" input-id="calendar-color" :error="errors.color">
+                    <Input id="calendar-color" v-model="newCalendar.color" type="color" />
                 </FormGroup>
                 <div class="flex flex-col gap-3">
                     <Checkbox
+                        id="add-hide-from-public"
                         :model-value="!newCalendar.visible_to_public"
                         @update:model-value="newCalendar.visible_to_public = !$event"
                         label="Hide calendar from public view"
                     />
                     <Checkbox
+                        id="add-hide-details-from-public"
                         :model-value="!newCalendar.show_details_to_public"
                         @update:model-value="newCalendar.show_details_to_public = !$event"
                         label="Hide event details for public view (show as time blocks only)"
