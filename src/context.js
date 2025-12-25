a47688e90d51fa7d73404c9b264f05893664fa4e
wajeht <58354193+wajeht@users.noreply.github.com>
2025-12-24 20:28:39 -0600

    refactor(context): include finalConfig in context creation for enhanced configuration management

--- src/context.js
+++ src/context.js
@@ -63,6 +63,7 @@ export function createContext(customConfig = {}) {
             models,
             errors,
             utils,
+            config: finalConfig,
             ICAL: icalLibrary,
         }),
     };
