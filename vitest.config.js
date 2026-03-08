74b38dfe1dc677bcfee4a40a80805fb435700d77
wajeht <58354193+wajeht@users.noreply.github.com>
2026-03-07 20:18:21 -0600

    chore: remove unnecessary environment variables for tests

--- vitest.config.js
+++ vitest.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
             APP_ENV: "test",
             APP_PORT: "80",
             APP_VUE_PORT: "3000",
-            PRODUCTION_SSH_URL: "calendar@420.247.0.365",
             SESSION_SECRET: "your-session-secret-change-in-production",
         },
         testTimeout: 10000,
